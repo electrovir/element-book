7362d3273f4ba3a64a11133a200ef284526a7679
electrovir <electrovir@users.noreply.github.com>
2023-05-03 11:14:06 +0000

    take up full height in book app

--- packages/element-book/src/ui/elements/element-book-app.element.ts
+++ packages/element-book/src/ui/elements/element-book-app.element.ts
@@ -47,8 +47,8 @@ export const ElementBookApp = defineElement<{
         }
 
         .root {
-            max-height: 100%;
-            max-width: 100%;
+            height: 100%;
+            width: 100%;
             display: flex;
             position: relative;
         }
